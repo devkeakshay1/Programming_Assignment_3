37ef8ad0308609d6e6952817bd4b17463d06348a
devkeakshay1 <64247610+devkeakshay1@users.noreply.github.com>
2022-11-28 22:08:18 +0530

    Add files via upload

---
+++ Question3.py
@@ -0,0 +1,14 @@
+### 3. Write a Python Program to Check Leap Year?
+##### Sol.
+
+year=int(input("Enter year : "))
+
+if (year%4==0):
+    print("The year {} is leap year".format(year))
+else:
+    print("year {} is not leap year".format(year))
+
+###################################################
+#########ANSWER#################################
+#                Enter year : 2020
+#                The year 2020 is leap year
